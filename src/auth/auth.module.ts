bd20d2a53ea73bb3fa0099cc601ce0df3f209c76
Andriimetamindz <andriiivan@metamindz.co.uk>
2023-07-20 23:08:19 +0300

    auth module add jwt, config, userService

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,10 +2,14 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { PrismaService } from '../prisma.service';
+import { JwtModule } from '@nestjs/jwt';
+import { UserService } from '../user/user.service';
+import { ConfigService } from '@nestjs/config';
+
 @Module({
-  imports: [],
+  imports: [JwtModule.register({})],
   controllers: [AuthController],
-  providers: [AuthService, PrismaService],
+  providers: [AuthService, PrismaService, UserService, ConfigService],
   exports: [AuthService],
 })
 export class AuthModule {}
