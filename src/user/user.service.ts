791c3869e671f3732d1935251470b19896ed4c63
Andriimetamindz <andriiivan@metamindz.co.uk>
2023-07-20 21:49:18 +0300

    create user.service method

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -5,7 +5,16 @@ import { User } from '@prisma/client';
 @Injectable()
 export class UserService {
   constructor(private prismaService: PrismaService) {}
-
+  async create(userDto): Promise<User> {
+    const existUser = this.findByEmail(userDto.email);
+    if (existUser) {
+      throw new NotFoundException('User is already exist!');
+    }
+    const user = this.prismaService.user.create({
+      data: userDto,
+    });
+    return user
+  }
   async findById(id: string): Promise<User> {
     const user = this.prismaService.user.findUnique({
       where: { id },
