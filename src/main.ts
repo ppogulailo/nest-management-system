b0e627137f385c4eda8643ed458952d3b6835de5
Andriimetamindz <andriiivan@metamindz.co.uk>
2023-07-20 21:14:00 +0300

    add swagger

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import * as cookieParser from 'cookie-parser';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
@@ -13,6 +14,14 @@ async function bootstrap() {
     }),
   );
   app.use(cookieParser());
+  const config = new DocumentBuilder()
+    .setTitle('Scrapper API')
+    .setDescription('Scrapper api documentation for vacation')
+    .setVersion('1.0')
+    .addCookieAuth('optional-session-id')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 }
 bootstrap();
