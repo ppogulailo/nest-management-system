ffe5f819a15dcdd8977b9485577dbea641c31e6e
Andriimetamindz <andriiivan@metamindz.co.uk>
2023-07-20 21:37:34 +0300

    add user.module to app.mod

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,7 @@
 import { Module } from '@nestjs/common';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [],
+  imports: [UserModule],
 })
 export class AppModule {}
