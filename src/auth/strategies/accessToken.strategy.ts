f006acfd85196ac414feb876ce5bde93754c9a3b
Andriimetamindz <andriiivan@metamindz.co.uk>
2023-07-20 22:09:33 +0300

    auth strategies

---
+++ src/auth/strategies/accessToken.strategy.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+
+type JwtPayload = {
+  sub: string;
+  email: string;
+  name: string;
+  id: string;
+};
+
+@Injectable()
+export class AccessTokenStrategy extends PassportStrategy(Strategy, 'jwt') {
+  constructor(private readonly configService: ConfigService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: true,
+      secretOrKey: configService.get('JWT_ACCESS_SECRET'),
+    });
+  }
+
+  validate(payload: JwtPayload) {
+    return payload;
+  }
+}
