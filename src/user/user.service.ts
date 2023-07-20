53f4ba0631d632ceeb168a4d6db77362ca6f9700
Andriimetamindz <andriiivan@metamindz.co.uk>
2023-07-20 22:29:51 +0300

    updateRole user.service

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { PrismaService } from '../prisma.service';
-import { User } from '@prisma/client';
+import { User, UserRole } from '@prisma/client';
 import { CreateUserDto } from './dto/create-user.dto';
 import { USER_EXIST, USER_NOT_FOUND } from './const/user.const';
 
@@ -36,6 +36,21 @@ export class UserService {
     if (!user) {
       throw new NotFoundException(USER_NOT_FOUND);
     }
+    return user;
+  }
+  async updateRole(id: string, role: UserRole): Promise<User> {
+    const existUser = this.findById(id);
+    if (!existUser) {
+      throw new NotFoundException(USER_NOT_FOUND);
+    }
+    const user = this.prismaService.user.update({
+      where: {
+        id,
+      },
+      data: {
+        role,
+      },
+    });
     return user;
   }
 }
