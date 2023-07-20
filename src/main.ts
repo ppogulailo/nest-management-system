95d97d59bbfcbf077b3260a74b467d09a4583838
Andriimetamindz <andriiivan@metamindz.co.uk>
2023-07-20 21:15:02 +0300

    .env to .gitingnore

--- src/main.ts
+++ src/main.ts
@@ -22,6 +22,6 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 5000);
 }
 bootstrap();
