72ab474f5bce9539d2166ba59b306853666015f3
Andriimetamindz <andriiivan@metamindz.co.uk>
2023-07-20 22:33:26 +0300

    update role to controller, update-role.dto

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,9 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Param, Patch, Put } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { UserService } from './user.service';
+import { UpdateRoleDto } from './dto/update-role.dto';
 
 @ApiTags('User')
 @Controller('user')
 export class UserController {
   constructor(private userService: UserService) {}
+
+  @Put(':id')
+  async updateRole(@Param('id') id: string, @Body() { role }: UpdateRoleDto) {
+    return this.userService.updateRole(id, role);
+  }
 }
