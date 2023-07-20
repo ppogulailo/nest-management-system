c06ae854205743cab722bde83b84289a80adc43c
Andriimetamindz <andriiivan@metamindz.co.uk>
2023-07-20 21:56:44 +0300

    create.user method

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,17 +1,21 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { PrismaService } from '../prisma.service';
 import { User } from '@prisma/client';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Injectable()
 export class UserService {
   constructor(private prismaService: PrismaService) {}
-  async create(userDto): Promise<User> {
+  async create(userDto: CreateUserDto): Promise<User> {
     const existUser = this.findByEmail(userDto.email);
     if (existUser) {
       throw new NotFoundException('User is already exist!');
     }
     const user = this.prismaService.user.create({
-      data: userDto,
+      data: {
+        ...userDto,
+        role: 'User',
+      },
     });
     return user;
   }
