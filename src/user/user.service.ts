29ee53f61b00222cce0bf7032af4be000eba92cc
Andriimetamindz <andriiivan@metamindz.co.uk>
2023-07-20 21:44:46 +0300

    findById user

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,6 +1,18 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { PrismaService } from '../prisma.service';
+import { User } from '@prisma/client';
 
 @Injectable()
 export class UserService {
-  constructor() {}
+  constructor(private prismaService: PrismaService) {}
+
+  async findById(id: string): Promise<User> {
+    const user = this.prismaService.user.findUnique({
+      where: { id },
+    });
+    if (!user) {
+      throw new NotFoundException('User not found!');
+    }
+    return user
+  }
 }
